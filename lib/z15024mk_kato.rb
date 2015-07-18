7c95fc7ff737d287ad997c6879c03b42b3798651
Masayuki Kato <xianx.heartbraker@me.com>
2015-07-18 12:52:51 +0000

    confirm

--- lib/z15024mk_kato.rb
+++ lib/z15024mk_kato.rb
@@ -1,5 +1,21 @@
 require "z15024mk_kato/version"
+require "pebbles-soreyuke"
 
-module Z15024mkKato
-  # Your code goes here...
+
+module Z15025mkKato
+  def soreyuke(count)
+    (1..count).each do |num|
+      if num % 15 == 0
+          puts Pebbles::Soreyuke.AA('apm', 'FizzBuzz')            
+      elsif num % 5 == 0
+          puts Pebbles::Soreyuke.AA('spm', 'Buzz')
+      elsif num % 3 == 0
+          puts Pebbles::Soreyuke.AA('cpm', 'FIzz')
+      else
+          puts num
+      end
+    end
+  end
+  
+  module_function :soreyuke
 end
